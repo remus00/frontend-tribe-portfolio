bb91e342948da530df7b4af7f142cb9675ae518b
remus-wolico <135813551+remus00@users.noreply.github.com>
2024-12-15 21:05:54 +0100

    setup the font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,36 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import { cn } from '@/lib/utils';
+import type { Metadata } from 'next';
+import { Archivo } from 'next/font/google';
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: "Minimal Single Page Portfolio",
-  description: "Created with Frontend Tribe",
+    title: 'Minimal Single Page Portfolio',
+    description: 'Created with Frontend Tribe',
+    authors: [{ name: 'Marian Remus Burlacu' }],
 };
 
+const archivo = Archivo({
+    display: 'swap',
+    subsets: ['latin'],
+    variable: '--font-archivo',
+    weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+});
+
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={"antialiased"}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body
+                className={cn(
+                    'bg-stone-200 font-sans text-stone-900 antialiased',
+                    archivo.variable
+                )}
+            >
+                {children}
+            </body>
+        </html>
+    );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,17 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
     content: [
-        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/sections/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/sections/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     ],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: 'var(--font-archivo)',
+            },
+        },
     },
     plugins: [],
 };
