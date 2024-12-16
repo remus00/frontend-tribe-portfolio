71ca0d0c246203869aecb347c8236e715d063e08
remus-wolico <135813551+remus00@users.noreply.github.com>
2024-12-16 09:48:33 +0100

    intro section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Header } from '@/components/sections/header';
 import { Hero } from '@/components/sections/hero';
+import { Intro } from '@/components/sections/intro';
 
 export default function Home() {
     return (
         <>
             <Header />
             <Hero />
+            <Intro />
         </>
     );
 }
---
+++ src/components/sections/intro.tsx
@@ -0,0 +1,14 @@
+import { SectionWrapper } from '../custom/section-wrapper';
+
+export const Intro = () => {
+    return (
+        <SectionWrapper className="mt-12 md:mt-16 md:py-32 lg:mt-20 lg:py-40">
+            <div className="container">
+                <h2 className="text-4xl md:text-7xl lg:w-[80%] lg:text-8xl">
+                    Building beautiful websites with clean code and thoughtful design to
+                    help your business grow and stand out online
+                </h2>
+            </div>
+        </SectionWrapper>
+    );
+};
