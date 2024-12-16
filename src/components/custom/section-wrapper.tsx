8bf51935e104756b645b3cd7a731d2f00c209f83
remus-wolico <135813551+remus00@users.noreply.github.com>
2024-12-16 10:02:36 +0100

    fix section wrapper classes

--- src/components/custom/section-wrapper.tsx
+++ src/components/custom/section-wrapper.tsx
@@ -8,7 +8,7 @@ interface Props extends HTMLAttributes<HTMLDivElement> {
 
 export const SectionWrapper = ({ children, className, ...rest }: Props) => {
     return (
-        <section className={cn('py-24', className)} {...rest}>
+        <section className={cn('py-24 md:py-32 lg:py-40', className)} {...rest}>
             {children}
         </section>
     );
